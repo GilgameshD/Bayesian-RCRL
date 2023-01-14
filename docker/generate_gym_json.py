84aa4ce193dcc8a0c92d58241156ce8e4fd517b0
GilgameshD <wenhaod@andrew.cmu.edu>
2023-01-13 23:46:50 -0500

    update

--- docker/generate_gym_json.py
+++ docker/generate_gym_json.py
@@ -2,7 +2,7 @@
 Author: Wenhao Ding
 Email: wenhaod@andrew.cmu.edu
 Date: 2022-08-18 03:14:33
-LastEditTime: 2023-01-13 22:57:34
+LastEditTime: 2023-01-13 23:45:48
 Description: 
 '''
 
@@ -11,7 +11,7 @@ import json
 
 
 command = """ \
-    mv /workspace/Bayesian-DQN/.mujoco /root/.mujoco && \
+    sudo apt-get install libosmesa6-dev && \
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin && \
     pip install -e /workspace/Bayesian-DQN/d3rlpy && \
     pip install -e /workspace/Bayesian-DQN/d4rl && \
